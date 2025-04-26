81d0a50b8ceff2d309798379d498136ff6ea88e1
wazpx <stamatiou917@gmail.com>
2025-04-26 15:53:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,102 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+import express from 'express';
+import cors from 'cors';
+import http from 'node:http';
+import path from 'node:path';
+import { hostname } from 'node:os';
+import chalk from 'chalk';
+import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
+import { epoxyPath } from '@mercuryworkshop/epoxy-transport';
+import { libcurlPath } from '@mercuryworkshop/libcurl-transport';
+import { baremuxPath } from '@mercuryworkshop/bare-mux/node';
+import { server as wisp } from '@mercuryworkshop/wisp-js/server';
+import routes from './src/routes.js';
 
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+const server = http.createServer();
+const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+	app.set('trust proxy', true);
+}
 
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    });
+const __dirname = process.cwd();
+const PORT = process.env.PORT || 6060;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/epoxy/', express.static(epoxyPath));
+app.use('/@/', express.static(uvPath));
+app.use('/libcurl/', express.static(libcurlPath));
+app.use('/baremux/', express.static(baremuxPath));
+
+app.use('/', routes);
+
+server.on('request', (req, res) => {
+	app(req, res);
 });
 
-function isUrl(val = ''){
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
+server.on('upgrade', (req, socket, head) => {
+	if (req.url.endsWith('/wisp/')) {
+		wisp.routeRequest(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
 
-function quickLink(url1) {
-    window.location.href = __uv$config.prefix + url1;
-}
+server.on('listening', () => {
+	const address = server.address();
+	const theme = chalk.hex('#8F00FF');
+	const host = chalk.hex('0d52bd');
+	console.log(
+		chalk.bold(
+			theme(`
+	███████╗██████╗  █████╗  ██████╗███████╗
+	██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝
+	███████╗██████╔╝███████║██║     █████╗  
+	╚════██║██╔═══╝ ██╔══██║██║     ██╔══╝  
+	███████║██║     ██║  ██║╚██████╗███████╗
+	╚══════╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝
+											
+	`)
+		)
+	);
+	console.log(
+		`  ${chalk.bold(host('Local System:'))}            http://${address.family === 'IPv6' ? `[${address.address}]` : address.address}${address.port === 80 ? '' : ':' + chalk.bold(address.port)}`
+	);
+
+	console.log(
+		`  ${chalk.bold(host('Local System:'))}            http://localhost${address.port === 8080 ? '' : ':' + chalk.bold(address.port)}`
+	);
+
+	try {
+		console.log(
+			`  ${chalk.bold(host('On Your Network:'))}  http://${hostname()}${address.port === 8080 ? '' : ':' + chalk.bold(address.port)}`
+		);
+	} catch (err) {
+		// can't find LAN interface
+	}
+
+	if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
+		console.log(
+			`  ${chalk.bold(host('Replit:'))}           https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+		);
+	}
+
+	if (process.env.HOSTNAME && process.env.GITPOD_WORKSPACE_CLUSTER_HOST) {
+		console.log(
+			`  ${chalk.bold(host('Gitpod:'))}           https://${PORT}-${process.env.HOSTNAME}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
+		);
+	}
+
+	if (
+		process.env.CODESPACE_NAME &&
+		process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN
+	) {
+		console.log(
+			`  ${chalk.bold(host('Github Codespaces:'))}           https://${process.env.CODESPACE_NAME}-${address.port === 80 ? '' : address.port}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
+		);
+	}
+});
+
+server.listen(PORT);
